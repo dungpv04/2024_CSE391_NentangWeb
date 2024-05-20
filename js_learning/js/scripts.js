f40cc9a746e81429616c7353153f27facf535b07
vandu <vandu@DESKTOP-CKTBGDM>
2024-05-20 13:58:22 +0700

    js_learning commit

---
+++ js_learning/js/scripts.js
@@ -0,0 +1,28 @@
+function SubmitHandle(){
+    
+    var value = document.getElementById('keyword').value;
+    var old_img = document.getElementById('image');
+
+    if(old_img != null) old_img.remove();
+    var img = document.createElement('img');
+    img.id = "image";
+    var container = document.getElementById('image_container');
+
+    if(value === "pepe"){
+        img.src = "..\\img\\pepe.jpg";
+        container.appendChild(img);
+    }
+    else if(value === "doraemon"){
+        img.src = "..\\img\\doraemon.jpg";
+        container.appendChild(img);
+    }
+    else if(value === "lemon"){
+        img.src = "..\\img\\lemon.jpg";
+        container.appendChild(img);
+    }
+    else{
+        img.src = "..\\img\\no_image.png";
+        container.appendChild(img);
+    }
+    console.log(value);
+}
